b62b515ee90a8f1d715ea9328158c44ea1653447
Hamish <hamish@pageland.co.nz>
2018-05-30 21:05:50 +1200

    Added in a race timer

---
+++ Assets/Scripts/LapTimerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapTimerScript : MonoBehaviour {
+
+    public float lapTimer = 0f;
+    public int lapCount = 0;
+    public bool raceGoing = false;
+
+    public Dictionary<int, float> lapTimes = new Dictionary<int, float>();
+
+	// Update is called once per frame
+	void Update () {
+		if (raceGoing)
+        {
+            lapTimer += Time.deltaTime;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (raceGoing)
+        {
+            lapTimes.Add(lapCount, lapTimer);
+            lapTimer = 0;
+            lapCount += 1;
+        }
+        else
+        {
+            raceGoing = true;
+        }
+    }
+}
