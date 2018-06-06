132675adc235fc0b1d13561af70df37d41feae8d
Hamish <hamish@pageland.co.nz>
2018-06-07 11:03:33 +1200

    Tested out the track
    Also put some music etc into the games files to add in later

--- Assets/Scripts/CameraTrackingScript.cs
+++ Assets/Scripts/CameraTrackingScript.cs
@@ -19,34 +19,37 @@ public class CameraTrackingScript : MonoBehaviour {
 
     private void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, Target.transform.position, moveStiffness * Time.deltaTime);
-
-        if (rotationLinked)
+        if (Target != null)
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Target.transform.rotation, rotationStiffness * Time.deltaTime);
-        }
-        else
-        {
-            /*
-            Quaternion zOnlyRot = Quaternion.Euler(new Vector3(transform.position.x, transform.position.y,Target.transform.rotation.eulerAngles.z));
-            transform.rotation = Quaternion.Lerp(transform.rotation, zOnlyRot, zRotStiffness * Time.deltaTime);
-            Quaternion xOnlyRot = Quaternion.Euler(new Vector3(Target.transform.rotation.eulerAngles.x, transform.position.y, transform.position.z));
-            transform.rotation = Quaternion.Lerp(transform.rotation, xOnlyRot, xRotStiffness * Time.deltaTime);
-            Quaternion yOnlyRot = Quaternion.Euler(new Vector3(transform.position.x, Target.transform.rotation.eulerAngles.y, transform.position.z));
-            transform.rotation = Quaternion.Lerp(transform.rotation, yOnlyRot, yRotStiffness * Time.deltaTime);
-            */
-            
-            Vector3 currRotEuler = transform.rotation.eulerAngles;
-            Vector3 targetRotEuler = Target.transform.rotation.eulerAngles;
+            transform.position = Vector3.Lerp(transform.position, Target.transform.position, moveStiffness * Time.deltaTime);
 
-            float xRot = Mathf.LerpAngle(currRotEuler.x, targetRotEuler.x, xRotStiffness * Time.deltaTime);
-            float yRot = Mathf.LerpAngle(currRotEuler.y, targetRotEuler.y, yRotStiffness * Time.deltaTime);
-            float zRot = Mathf.LerpAngle(currRotEuler.z, targetRotEuler.z, zRotStiffness * Time.deltaTime);
+            if (rotationLinked)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Target.transform.rotation, rotationStiffness * Time.deltaTime);
+            }
+            else
+            {
+                /*
+                Quaternion zOnlyRot = Quaternion.Euler(new Vector3(transform.position.x, transform.position.y,Target.transform.rotation.eulerAngles.z));
+                transform.rotation = Quaternion.Lerp(transform.rotation, zOnlyRot, zRotStiffness * Time.deltaTime);
+                Quaternion xOnlyRot = Quaternion.Euler(new Vector3(Target.transform.rotation.eulerAngles.x, transform.position.y, transform.position.z));
+                transform.rotation = Quaternion.Lerp(transform.rotation, xOnlyRot, xRotStiffness * Time.deltaTime);
+                Quaternion yOnlyRot = Quaternion.Euler(new Vector3(transform.position.x, Target.transform.rotation.eulerAngles.y, transform.position.z));
+                transform.rotation = Quaternion.Lerp(transform.rotation, yOnlyRot, yRotStiffness * Time.deltaTime);
+                */
 
-            currRotEuler = new Vector3(xRot, yRot, zRot);
+                Vector3 currRotEuler = transform.rotation.eulerAngles;
+                Vector3 targetRotEuler = Target.transform.rotation.eulerAngles;
 
-            transform.rotation = Quaternion.Euler(currRotEuler);
-            
+                float xRot = Mathf.LerpAngle(currRotEuler.x, targetRotEuler.x, xRotStiffness * Time.deltaTime);
+                float yRot = Mathf.LerpAngle(currRotEuler.y, targetRotEuler.y, yRotStiffness * Time.deltaTime);
+                float zRot = Mathf.LerpAngle(currRotEuler.z, targetRotEuler.z, zRotStiffness * Time.deltaTime);
+
+                currRotEuler = new Vector3(xRot, yRot, zRot);
+
+                transform.rotation = Quaternion.Euler(currRotEuler);
+
+            }
         }
     }
 }
---
+++ Assets/Scripts/MenuCamScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamScript : MonoBehaviour {
+
+    public List<Transform> camNodes;
+    public Transform menuSlots;
+    int currentNode = 0;
+
+    public CameraTrackingScript camTracking;
+
+    public float coolDownTime = 1f;
+    public bool onCoolDown;
+
+    private void Start()
+    {
+        Transform[] camTransforms = menuSlots.GetComponentsInChildren<Transform>();
+        camNodes = new List<Transform>();
+
+        for (int i = 0; i < camTransforms.Length; i++)
+        {
+            if (camTransforms[i] != menuSlots)
+            {
+                camNodes.Add(camTransforms[i]);
+            }
+        }
+    }
+
+    private void Update()
+    {
+
+        if (ControllerMapping.instance.HorizontalMovement() > 0.2f && onCoolDown == false)
+        {
+            NextItem();
+        }
+        if (ControllerMapping.instance.HorizontalMovement() < -0.2f && onCoolDown == false)
+        {
+            PrevItem();
+        }
+        if (ControllerMapping.instance.GetSelectDown())
+        {
+            Debug.Log("Selecting Item!");
+            camNodes[currentNode].GetComponent<CamPosScript>().OnSelection.Invoke();
+        }
+    }
+
+    public void NextItem()
+    {
+        StartCoroutine(StartCoolDown());
+        currentNode += 1;
+        if (currentNode >= camNodes.Count)
+        {
+            currentNode = 0;
+        }
+        camTracking.Target = camNodes[currentNode].gameObject;
+    }
+
+    public void PrevItem()
+    {
+        StartCoroutine(StartCoolDown());
+        currentNode -= 1;
+        if (currentNode < 0)
+        {
+            currentNode = camNodes.Count - 1;
+        }
+        camTracking.Target = camNodes[currentNode].gameObject;
+    }
+
+    IEnumerator StartCoolDown()
+    {
+        onCoolDown = true;
+        yield return new WaitForSeconds(coolDownTime);
+        onCoolDown = false;
+    }
+}
