bac97b5e9f5e8070649ba1938f44d1512b6e5d3e
Hamish <hamish@pageland.co.nz>
2018-06-21 09:13:39 +1200

    Added in a matswitching script for screen shots

---
+++ Assets/Scripts/Pretty Scripts/MatSwitchingScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatSwitchingScript : MonoBehaviour {
+
+    public SkinnedMeshRenderer skinnedRenderer;
+    public float swapTime = 0.5f;
+    public bool swapping = true;
+
+    public List<Material> swappingMats;
+    private int curMat;
+
+	// Use this for initialization
+	void Start () {
+        curMat = 0;
+        //SwapMat();
+        StartCoroutine(RunSwapping());
+	}
+
+    IEnumerator RunSwapping()
+    {
+        while (swapping)
+        {
+            yield return new WaitForSeconds(swapTime);
+            SwapMat();
+        }
+    }
+	
+    void SwapMat()
+    {
+        curMat += 1;
+        if (curMat >= swappingMats.Count)
+        {
+            curMat = 0;
+        }
+        Debug.Log(curMat);
+
+        Material[] mats = skinnedRenderer.materials;
+
+        mats[1] = swappingMats[curMat];
+        skinnedRenderer.materials = mats;
+    }
+
+}
